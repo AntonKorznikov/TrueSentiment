1e2d2b973ae41258a34184261723f9268ecbc26c
Anton Korznikov <korznikovantona@gmail.com>
2024-10-24 17:44:03 +0300

    Update processing.py

--- processing/processing.py
+++ processing/processing.py
@@ -10,13 +10,16 @@ OUTPUT_RAW_DIR = 'examples/output_raw'
 def process():
     if not os.path.exists(OUTPUT_RAW_DIR):
         os.makedirs(OUTPUT_RAW_DIR)
-    classifier = pipeline('sentiment-analysis')
+    # classifier = pipeline('sentiment-analysis')
     for filename in os.listdir(INPUT_DIR):
         if filename.endswith('.txt'):
             with open(os.path.join(INPUT_DIR, filename), 'r') as f:
                 text = f.read()
-            result = classifier(text)[0]
-            with open(os.path.join(OUTPUT_RAW_DIR, f"{filename}.json"), 'w') as f:
+            print(1)
+            # result = classifier(text)[0]
+            result = {'label': 'POSITIVE', 'score': 0.9998}
+            print(2)
+            with open(os.path.join(OUTPUT_RAW_DIR, f"{filename[:-4]}.json"), 'w') as f:
                 json.dump(result, f)
     print("Processing completed.")
 
